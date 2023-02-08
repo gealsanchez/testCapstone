26aa9c0f3110fdfc3160d08f8fdc01edc4b05a03
anderCM2 <ginoander_5@hotmail.com>
2023-02-08 14:16:56 -0500

    Meals counter added

--- src/modules/mealUI.js
+++ src/modules/mealUI.js
@@ -1,13 +1,17 @@
 const URL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ooCKDNVhVwx5nuC5j8LT/likes/';
 import getMeals from './getMeals.js';
 import mealCard from './mealCard.js';
+import mealCounter from './mealCounter.js';
+
 
 const mealUI = async () => {
   const response = await fetch(URL);
   const likes = await response.json();
 
   const data = await getMeals();
-  console.log(data);
+  
+  mealCounter(data.meals.length);
+
   data.meals.forEach((meal) => {
     let likeNum = 0;
     likes.forEach((item) => {
